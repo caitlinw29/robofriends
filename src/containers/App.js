3bd4bba37f7cce0ab820115b56de421b35b7bdf3
Caitlin Wall <caitlinwall96@gmail.com>
2023-07-04 19:42:43 -0400

    Refactor App.js; Add ErrorBoundary; Add pointer on hover to Card

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundary from '../components/ErrorBoundary';
 
 class App extends Component {
   constructor(){
@@ -24,23 +25,23 @@ class App extends Component {
   }
 
   render() {
-    const filteredRobots = this.state.robots.filter(robot => {
-      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     })
-    if(this.state.robots.length===0) {
-      return <h1>Loading</h1>
-    } else {
-      return (
-        <div className='tc'>
-          <h1 className='f1'>RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange}/>
-          <Scroll>
+    return !robots.length ? 
+      <h1>Loading</h1> :
+    (
+      <div className='tc'>
+        <h1 className='f1'>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange}/>
+        <Scroll>
+          <ErrorBoundary>
             <CardList robots={filteredRobots}/>
-          </Scroll>
-          
-        </div>
-      );
-    }
+          </ErrorBoundary>
+        </Scroll>
+      </div>
+    );
   }
 }
 
